0becdaac887d824b9255c7eb64bdd112fe2ee2da
NetLord8 <ilhom.ulmasov@gmail.com>
2016-06-07 11:31:03 +0300

    test

--- src/com/il/test/test.java
+++ src/com/il/test/test.java
@@ -1,15 +1,23 @@
 package com.il.test;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Iterator;
 import java.util.List;
 
@@ -99,46 +107,26 @@ public class test extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/medtest");
+            Connection conn = ds.getConnection();
 
-        response.setContentType("text/plain; charset=utf-8");
-        response.setCharacterEncoding("UTF-8");
+            Statement statement = conn.createStatement();
+            String sql = "select sysdate+1 as s from dual";
+            ResultSet rs = statement.executeQuery(sql);
 
-        FileInputStream file = new FileInputStream(new File("D:\\temp\\Onni.xlsm"));
+            int count = 1;
+            while (rs.next()) {
+                writer.println(rs.getDate("s"));
 
-
-        XSSFWorkbook workbook = new XSSFWorkbook(file);
-        XSSFSheet sheet = workbook.getSheetAt(0);
-        Iterator<Row> rows = sheet.iterator();
-        PrintWriter out=response.getWriter();
-        //=response.getOutputStream();
-        while(rows.hasNext())
-        {
-            Row row=rows.next();
-            Cell cell=row.getCell(1);
-            int cellType = cell.getCellType();
-            switch (cellType) {
-                case Cell.CELL_TYPE_STRING:
-                    System.out.print(cell.getStringCellValue() + "=");
-                    out.println(cell.getStringCellValue() + "=");
-                    break;
-                case Cell.CELL_TYPE_NUMERIC:
-                    System.out.print("[" + cell.getNumericCellValue() + "]");
-                    out.println("[" + cell.getNumericCellValue() + "]");
-                    break;
-
-                case Cell.CELL_TYPE_FORMULA:
-                    System.out.print("[" + cell.getNumericCellValue() + "]");
-                    out.println("[" + cell.getNumericCellValue() + "]");
-                    break;
-                default:
-                    System.out.print("|");
-
-                    out.println("|");
-                    break;
             }
-            //out.println(cell.getStringCellValue());
+        } catch (NamingException ex) {
+            System.err.println(ex);
+        } catch (SQLException ex) {
+            System.err.println(ex);
         }
-        out.print("huy");
-        out.close();
     }
 }
